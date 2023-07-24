d844b226abe50712a4e223b1baabe42e9101579f
EvgeniyaSarmina <139544566+EvgeniyaSarmina@users.noreply.github.com>
2023-07-24 22:26:56 +0500

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+//Инпуты
+let StudentSurname = document.querySelector('#Surname')
+let StudentName = document.querySelector('#Name')
+
+//Кнопки
+let addStudentButton = document.querySelector('.add-student')
+
+//Таблица
+let studentsListElement = document.querySelector('.students-list')
+
+let studentsArray = []
+
+addStudentButton.addEventListener('click', function(event) {
+    let StudentSurnameValue = StudentSurname.value
+    let StudentNameValue = StudentName.value
+
+    let studentsString = `${StudentSurnameValue} ${StudentNameValue}`
+    
+    studentsArray.push(studentsString)
+
+    console.log(studentsArray)
+    let innerHTMLWithStudents = ''
+
+    studentsArray.forEach((item, i) => {
+        innerHTMLWithStudents += `<div>${item} ${i+1}</div>`
+    })
+
+    studentsListElement.innerHTML = innerHTMLWithStudents
+})
+
